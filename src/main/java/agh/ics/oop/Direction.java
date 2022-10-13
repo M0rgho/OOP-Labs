3ae00e031820643c7f923a0af9c9e38efcbbad70
Mikołaj Maślak <95372995+M0rgho@users.noreply.github.com>
2022-10-13 12:49:52 +0200

    Edited Direction.java & World.java
    Added classes:
    - Vector
    Added enums:
    - MoveDirection
    - MapDirection

--- src/main/java/agh/ics/oop/Direction.java
+++ src/main/java/agh/ics/oop/Direction.java
@@ -6,3 +6,53 @@ enum Direction {
     RIGHT,
     LEFT
 }
+
+enum MoveDirection {
+    FORWARD,
+    BACKWARD,
+    RIGHT,
+    LEFT
+}
+
+enum MapDirection {
+    NORTH,
+    SOUTH,
+    EAST,
+    WEST;
+
+    public String toString(){
+        return switch (this) {
+            case NORTH -> "Północ";
+            case SOUTH -> "Południe";
+            case WEST -> "Południe";
+            case EAST -> "Południe";
+        };
+    }
+
+    public MapDirection next(MapDirection direction) {
+        return switch (direction) {
+            case NORTH -> WEST;
+            case EAST -> NORTH;
+            case SOUTH -> EAST;
+            case WEST -> SOUTH;
+        };
+    }
+
+    public MapDirection previous(MapDirection direction) {
+        return switch (direction) {
+            case NORTH -> EAST;
+            case EAST -> SOUTH;
+            case SOUTH -> WEST;
+            case WEST -> NORTH;
+        };
+    }
+    public Vector2d toUnitVector(MapDirection direction) {
+        return switch (direction) {
+            case NORTH -> new Vector2d(0, 1);
+            case EAST -> new Vector2d(1, 0);
+            case SOUTH -> new Vector2d(0, -1);
+            case WEST -> new Vector2d(-1, 0);
+        };
+    }
+
+}
--- src/main/java/agh/ics/oop/World.java
+++ src/main/java/agh/ics/oop/World.java
@@ -1,5 +1,59 @@
 package agh.ics.oop;
 
+import javax.xml.stream.FactoryConfigurationError;
+
+class Vector2d {
+    final public int x;
+    final public int y;
+
+    Vector2d(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public String toString() {
+        return String.format("(%d,%d)", this.x, this.y);
+    }
+
+    boolean precedes(Vector2d other) {
+        return this.x <= other.x && this.y <= other.y;
+    }
+
+    boolean follow(Vector2d other) {
+        return this.x >= other.x && this.y >= other.y;
+    }
+
+    Vector2d add(Vector2d other) {
+        return new Vector2d(this.x + other.x, this.y + other.y);
+    }
+
+    Vector2d subtract(Vector2d other) {
+        return new Vector2d(this.x - other.x, this.y - other.y);
+    }
+
+    Vector2d upperRight(Vector2d other) {
+        return new Vector2d(Math.max(this.x, other.x), Math.max(this.y, other.y));
+    }
+
+    Vector2d lowerLeft(Vector2d other) {
+        return new Vector2d(Math.min(this.x, other.x), Math.min(this.y, other.y));
+    }
+
+    Vector2d oppsite() {
+        return new Vector2d(-this.x, -this.y);
+    }
+
+    public boolean equals(Object other) {
+        if (!(other instanceof Vector2d)) {
+            return false;
+        }
+        return this.x == other.x && this.y == other.y;
+    }
+
+
+
+}
+
 class World {
     private static Direction[] processDirections(String[] args) {
         Direction[] directions = new Direction[args.length];
