f5f2a69086763f74055ed2d9628454a78f69de7f
Mikolaj Maslak <nicholasdev7@gmail.com>
2022-11-29 20:37:20 +0100

    finished gui for lab8

--- src/main/java/agh/ics/oop/gui/App.java
+++ src/main/java/agh/ics/oop/gui/App.java
@@ -4,15 +4,14 @@ package agh.ics.oop.gui;
 import agh.ics.oop.*;
 import javafx.application.Application;
 import javafx.application.Platform;
-import javafx.geometry.HPos;
-import javafx.geometry.VPos;
-import javafx.scene.Node;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.*;
+import javafx.scene.paint.Color;
 import javafx.scene.text.Font;
 import javafx.scene.text.Text;
+import javafx.scene.text.TextAlignment;
 import javafx.stage.Stage;
 
 public class App extends Application implements IPositionChangeObserver{
@@ -26,25 +25,36 @@ public class App extends Application implements IPositionChangeObserver{
 
     private GridGui gridGui;
     private SimulationEngine engine;
+    private final Text infoText = new Text();
+    private final Button startButton = new Button("START");
 
-
+    private final TextField movesInput = new TextField();
     @Override
     public void start(Stage primaryStage) {
 
-        String[] args = getParameters().getRaw().toArray(new String[0]);
-        try {
-            runSimulation(args);
-        } catch (IllegalArgumentException e) {
-            System.err.println(e.getMessage());
-        }
-        Button startButton = new Button("START");
+        prepareEngine();
+
+
+        movesInput.setPromptText("Enter moves");
+        infoText.setFont(new Font(14));
+        infoText.setTextAlignment(TextAlignment.CENTER);
         startButton.setOnAction(event -> {
-            startButton.setDisable(true);
-            startSimulation();
+            try {
+                MoveDirection[] direction = OptionsParser.parse(movesInput.getCharacters().toString().split("\\s+"));
+                engine.setDirections(direction);
+                startButton.setDisable(true);
+                startSimulation();
+            } catch (IllegalArgumentException e) {
+                infoText.setStroke(Color.RED);
+                infoText.setText("Invalid move: " + e.getMessage());
+                System.err.println(e.getMessage());
+            }
         });
-        HBox navPanel = new HBox(startButton, new TextField());
+
+
+        HBox navPanel = new HBox(5, startButton, movesInput, infoText);
         gridGui = new GridGui(map, cellSize);
-        VBox mainElement = new VBox(navPanel, gridGui);
+        VBox mainElement = new VBox(5, navPanel, gridGui);
 
         Scene scene = new Scene(mainElement, appWidth, appHeight);
 //        primaryStage.setResizable(false);
@@ -54,18 +64,32 @@ public class App extends Application implements IPositionChangeObserver{
     }
 
     private void startSimulation() {
-        new Thread(engine::run).start();
+        infoText.setText("");
+        new Thread(() -> {
+            Thread t = new Thread(engine::run);
+            try {
+                t.start();
+                t.join();
+                infoText.setText("Simulation has run successfully");
+                infoText.setStroke(Color.GREEN);
+                startButton.setDisable(false);
+            } catch (InterruptedException e) {
+                System.err.println("Thread execution was abrupted");
+                infoText.setStroke(Color.RED);
+                infoText.setText("Simulation error: " + e.getMessage());
+            }
+
+        }).start();
     }
 
-    private void runSimulation(String[] args) {
+    private void prepareEngine() {
         int grassCount = 10;
-        MoveDirection[] directions = OptionsParser.parse(args);
         map = new GrassField(grassCount);
-        Vector2d[] positions = {new Vector2d(2, 2), new Vector2d(2, 3)};
-
-        engine = new ThreadedSimulationEngine(directions, map, positions, moveDelay);
+        Vector2d[] positions = {new Vector2d(4, 4), new Vector2d(6, 4)};
+        engine = new ThreadedSimulationEngine(new MoveDirection[]{}, map, positions, moveDelay);
         engine.addObserver(this);
     }
+
 
 
     @Override
